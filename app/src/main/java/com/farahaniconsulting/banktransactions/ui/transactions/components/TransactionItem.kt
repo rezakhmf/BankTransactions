c0a3231a9ccb2b506131338cc4d753c3c6ff8d27
Reza Farahani <reza.khmf@gmail.com>
2023-11-25 23:55:27 +1100

    feature-layout-cleanup-impors

--- app/src/main/java/com/farahaniconsulting/banktransactions/ui/transactions/components/TransactionItem.kt
+++ app/src/main/java/com/farahaniconsulting/banktransactions/ui/transactions/components/TransactionItem.kt
@@ -2,27 +2,18 @@ package com.farahaniconsulting.banktransactions.ui.transactions.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.farahaniconsulting.banktransactions.domain.model.Transaction
 import com.farahaniconsulting.banktransactions.ui.common.getCategoryIcon
